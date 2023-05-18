92de0ec846fcdcb1549d9c8166fdb14be0eac2d2
Sangy K <sansk@users.noreply.github.com>
2023-05-18 12:21:50 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./components/loading/loading";
+import Tours from "./components/tours/tours";
 
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const toursData = await response.json();
+
+      setIsLoading(false);
+      setTours(toursData);
+    } catch (e) {
+      setIsLoading(false);
+      console.log("Error: ", e);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
   return (
-    <h2>
-      <Loading />
-    </h2>
+    <main>
+      <Tours tours={tours} />
+    </main>
   );
 }
 
