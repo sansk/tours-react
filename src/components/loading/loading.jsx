b7acd3f0bd5daa5c9b2cc5c720663abd77596400
Sangy K <sansk@users.noreply.github.com>
2023-05-18 11:36:31 +0000

    Update 5 files

---
+++ src/components/loading/loading.jsx
@@ -0,0 +1,16 @@
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div class="loadingio-spinner-dual-ring-3hdbbqu1ois">
+      <div class="ldio-kqf3yzs7zu">
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
